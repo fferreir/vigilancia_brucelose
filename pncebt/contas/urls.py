c8fde11107e5c57b03842a31c484c5e7c13ef3a4
Fernando Ferreira <fernandoferreira@usp.br>
2025-04-10 20:59:22 -0300

    Adjustments to the login page

--- pncebt/contas/urls.py
+++ pncebt/contas/urls.py
@@ -3,6 +3,8 @@ from django.urls import include,path
 from . import views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('editar/', views.editar, name='editar'),
