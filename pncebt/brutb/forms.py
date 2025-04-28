78e46bb279d03eb76549e9ce96e1f726eb88e2c0
Fernando Ferreira <fernandoferreira@usp.br>
2025-04-28 15:58:05 -0300

    Start of brutb app development. Creation of the model for the Brazil database

---
+++ pncebt/brutb/forms.py
@@ -0,0 +1,14 @@
+# forms.py
+from django import forms
+from .models import Brasil
+
+class Propriedade(forms.Form):
+    municipios = forms.ModelChoiceField(queryset=Brasil.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super(Propriedade, self).__init__(*args, **kwargs)
+
+        if user and hasattr(user, 'perfil'):
+            estado_do_usuario = user.perfil.estado
+            self.fields['municipio'].queryset = Brasil.objects.filter(estado=estado_do_usuario)
--- pncebt/brutb/models.py
+++ pncebt/brutb/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Brasil(models.Model):
+    # Regular Django fields corresponding to the attributes in the
+    # Brazil's shapefile.
+    cd_mun = models.CharField(max_length=7)
+    nm_mun = models.CharField(max_length=100)
+    cd_uf = models.CharField(max_length=2)
+    nm_uf = models.CharField(max_length=50)
+
+    # GeoDjango-specific: a geometry field (MultiPolygonField)
+    geom = models.MultiPolygonField(srid=4674)
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.nm_mun
+
