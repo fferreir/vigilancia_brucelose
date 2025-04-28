f719aa1bd5d4f0aad5de795ee6371ecd56b37755
Fernando Ferreira <fernandoferreira@usp.br>
2025-04-28 16:28:52 -0300

    Added farm form

--- pncebt/brutb/views.py
+++ pncebt/brutb/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import Propriedade
 def home(request):
 
     return render(request, 'brutb/index.html')
 
 
+def propriedade_view(request):
+    if request.method == 'POST':
+        form = Propriedade(request.POST, user=request.user)
+        if form.is_valid():
+            # Processa os dados aqui
+            pass
+    else:
+        form = Prorpriedade(user=request.user)
+
+    return render(request, 'propriedade.html', {'form': form})
