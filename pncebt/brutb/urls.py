6f1bfc5d9706a9c526454537b13d81a8beec96fa
Fernando Ferreira <fernandoferreira@usp.br>
2025-05-17 22:56:32 -0300

    Implemented export to excel

--- pncebt/brutb/urls.py
+++ pncebt/brutb/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.lista_propriedades, name='lista_propriedades'),
     path('propriedade/lista/', views.lista_propriedades, name='lista_propriedades'),
     path('propriedade/adiciona/', views.propriedade, name='propriedade_adiciona'),
+    path('propriedade/exportar/', views.exportar_propriedades, name='exportar_propriedades'),
     path('propriedade/<str:cod_rebanho_estudo>/', views.propriedade_detalhe, name='propriedade_detalhe'),
+
 ]
--- pncebt/brutb/views.py
+++ pncebt/brutb/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Propriedade
 from .forms import Cadastro_Propriedade
+import datetime
+import openpyxl
+import io
+from django.db.models.fields.related import ForeignKey, OneToOneField
+from django.utils import timezone
+from django.http import HttpResponse
 
 @login_required
 def home(request):
@@ -43,3 +49,41 @@ def propriedade_detalhe(request, cod_rebanho_estudo):
     propriedade = get_object_or_404(Propriedade, cod_rebanho_estudo=cod_rebanho_estudo)
     return render(request, 'brutb/propriedade/detalhe.html', {'propriedade': propriedade})
 
+def exportar_propriedades(request):
+    # Obtém todas as propriedades
+    propriedades_lista = Propriedade.objects.select_related('veterinario').filter(estado=request.user.perfil.estado)
+    # Cria um arquivo Excel
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
+    # Define o nome da planilha
+    worksheet.title = 'Propriedades'
+    # Adiciona nome dos campos como cabeçalho das colunas
+    header = [field.name for field in Propriedade._meta.fields]
+    print(header)
+    # Adiciona cabeçalho
+    worksheet.append(header)
+    # Adiciona dados
+    for propriedade in propriedades_lista:
+        row_data = []
+        for field in Propriedade._meta.fields:
+            value = getattr(propriedade, field.name)
+            if isinstance(field, (ForeignKey, OneToOneField)):
+                value = str(value) if value else ""
+            if isinstance(value, (datetime.datetime, datetime.time)):
+                if timezone.is_aware(value):  # Maneira idiomática do Django
+                    value = value.replace(tzinfo=None)
+            row_data.append(value)
+        worksheet.append(row_data)
+
+    # Salva o arquivo Excel em memória
+    saida = io.BytesIO()
+    workbook.save(saida)
+    saida.seek(0)
+    # Retorna o arquivo em resposta HTTP
+    response = HttpResponse(
+        saida.read(),
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    # Configura o cabeçalho Content-Disposition para forçar o download e sugerir um nome de arquivo
+    response['Content-Disposition'] = 'attachment; filename="propriedades.xlsx"'
+    return response
